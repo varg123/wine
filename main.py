e693bc553490e7dda1b6e060715ccdbd3f933f01
petrovskij465 <petrovskij465@gmail.com>
2020-01-14 21:07:51 +0500

    Добавил возраст винодельни
    Установлена jinja2, просмотрен пример использования

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+import datetime
 
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html'])
+)
+
+template = env.get_template('template.html')
+
+year_of_creation = datetime.datetime(year=1920, month=1, day=1)
+age = datetime.datetime.now() - year_of_creation
+
+rendered_page = template.render(
+    age=int(age.days / 365.25)
+)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
